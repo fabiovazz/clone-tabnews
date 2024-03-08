2240791d9ceed5f3eefbd899b7a1ef23e135d836
Fabio Vaz <fabiohenriquevaz@hotmail.com>
2024-03-08 00:03:57 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Criação do clone tabnews.com.br, desenvolvido no curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-  return <h1>Olá, mundo!</h1>
+  return <h1>Olá, mundo!</h1>;
 }
 export default Home;
